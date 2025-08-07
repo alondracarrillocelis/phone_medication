955b925c53e3808bdf6802e80afbe10bb68fd75a
alondracarrillocelis <alondra_3127200055@utd.edu.mx>
2025-08-06 20:10:40 -0600

    adición de firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
     id("org.jetbrains.kotlin.kapt")
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -11,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.company.appmovil"
+        applicationId = "com.example.phone_medicatios"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -70,7 +71,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
     // Jetpack Compose core
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.6.7")
